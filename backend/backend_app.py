e94c369d6b88995b09dda8d74a4bb60c4cc6c513
Erik Kling <kling.erik@icloud.com>
2025-06-24 12:15:07 +0200

    Updated backend

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,55 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_posts():
+    data = request.get_json()
+    title = data.get('title')
+    content = data.get('content')
+
+    if not title or not content:
+        return jsonify({"error": "Title and content are required"}), 400
+    
+    if POSTS:
+        new_id = POSTS[-1]["id"] + 1
+    else:
+        new_id = 1
+
+    new_post = {"id": new_id, "title": title, "content": content}
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete_posts(id):
+    post_to_delete = next((post for post in POSTS if post['id'] == id), None)
+
+    if post_to_delete is None:
+        return jsonify({"error": "Post not found."}), 404
+    
+    POSTS.remove(post_to_delete)
+    return jsonify({"message": f"Post with id {id} has been deleted successfully."}), 200
+
+
+@app.route('/api/posts/<int:id>', methods=['PUT'])
+def update_post(id):
+    post_to_update = next((post for post in POSTS if post['id'] == id), None)
+    
+    if post_to_update is None:
+        return jsonify({"error": "Post not found."}), 404
+
+    data = request.get_json()
+    title = data.get('title')
+    content = data.get('content')
+
+    if title:
+        post_to_update['title'] = title
+    if content:
+        post_to_update['content'] = content
+
+    return jsonify(post_to_update), 200
 
 
 if __name__ == '__main__':
